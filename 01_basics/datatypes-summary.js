1a0a9acf74b4f55a7d8908b467bc21a2aa066bca
Vrajesh_Patoliya <vrajeshpatoliya50@gmail.com>
2024-08-23 14:48:24 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive
+// 7 types : String, Number, Boolean, null, Undefined, Symbol, BigInt
+const score = 100
+const scoreValue = 100.5
+
+const IsLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 5735732547857869n
+
+
+
+// Reference (Non Primitive)
+// Array, Objects, Functions
+const heros = ["shaktiman", "naagraj","doga"]
+let myObj = {
+    name: "Vrajesh",
+    age:20,
+}
+
+let myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3  // for read more

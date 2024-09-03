2d2d977b54fde7ce2809fd4946b11017f9f2a0e7
Vrajesh_Patoliya <vrajeshpatoliya50@gmail.com>
2024-09-03 14:19:26 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,35 @@ function loginUserMessage(username = "Vajju"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("Vrajesh"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Vajju",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 499
+})
+
+const myNewArray = [200, 100, 500, 700]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 100]));

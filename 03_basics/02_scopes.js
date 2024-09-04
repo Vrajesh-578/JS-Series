abbf848aa3632c726c7db2dc85bf3eb3a811e54b
Vrajesh_Patoliya <vrajeshpatoliya50@gmail.com>
2024-09-04 10:49:23 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,13 +5,51 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: ", a);   
+    // console.log("INNER: ", a);   
 }
 
-for(let i = 0; i < Array.length; i++){
-    const element = array[i];
-}
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Vajju"
+    
+    function two(){
+        const website = "Youtube"
+        // console.log(username);     
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if (true){
+    const username = "vrajesh"
+    if (username === "vrajesh"){
+        const website = "Youtube"
+        // console.log(username + website);    
+    }
+    // console.log(website);  
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}

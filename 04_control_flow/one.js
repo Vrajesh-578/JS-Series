697dfa458f048752c4da8af3037f9438f5948391
Vrajesh_Patoliya <vrajeshpatoliya50@gmail.com>
2024-09-09 11:17:28 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,36 @@
+// if
+const isUserLoggedIn = true
+const tempreture = 41
+
+// if(tempreture < 50){
+//   console.log("less than 50");    
+// } else
+// {
+// console.log("tempreture is greater than 50");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100){
+//    const power = "fly" 
+//    console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"),console.log("tets2");  // do not write like this (don't be over smart)
+
+if(balance < 500){
+    console.log("less than 500");
+} else if (balance < 750){
+    console.log("less than 750"); 
+} else if (balance < 900){
+    console.log("less than 900"); 
+} else{
+    console.log("less than 1200");  
+}

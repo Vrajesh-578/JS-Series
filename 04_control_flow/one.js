338ef1c1fffb911e47266bd6cf1f9edb7e89a2b7
Vrajesh_Patoliya <vrajeshpatoliya50@gmail.com>
2024-09-09 14:10:01 +0000

    conditions

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,6 +1,6 @@
 // if
-const isUserLoggedIn = true
-const tempreture = 41
+// const isUserLoggedIn = true
+// const tempreture = 41
 
 // if(tempreture < 50){
 //   console.log("less than 50");    
@@ -21,16 +21,29 @@ const tempreture = 41
 // console.log(`user power: ${power}`);
 
 
-const balance = 1000
+// const balance = 1000
 
 // if(balance > 500) console.log("test"),console.log("tets2");  // do not write like this (don't be over smart)
 
-if(balance < 500){
-    console.log("less than 500");
-} else if (balance < 750){
-    console.log("less than 750"); 
-} else if (balance < 900){
-    console.log("less than 900"); 
-} else{
-    console.log("less than 1200");  
+// if(balance < 500){
+//     console.log("less than 500");
+// } else if (balance < 750){
+//     console.log("less than 750"); 
+// } else if (balance < 900){
+//     console.log("less than 900"); 
+// } else{
+//     console.log("less than 1200");  
+// }
+
+const isUserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(isUserLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");   
 }
